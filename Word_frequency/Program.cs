1ce270b650767a4a86bc4542c763ec021b60160c
Ihor <igormykhno@gmail.com>
2021-10-21 13:59:07 +0300

    final version of Word Frequency

--- Word_frequency/Program.cs
+++ Word_frequency/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Word_frequency
 {
@@ -6,7 +7,29 @@ namespace Word_frequency
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string text = "Black red black";
+            Dictionary<string, int> wordFrequency = numberWordsInText(text);
+            PrintWordFrequency(wordFrequency);
+        }
+        static void PrintWordFrequency(Dictionary<string, int> wordFrequency)
+        {
+            foreach (var word in wordFrequency)
+            {
+                System.Console.WriteLine($"{word.Key}: {word.Value}");
+            }
+        }
+        static Dictionary<string, int> numberWordsInText(string text)
+        {
+            List<string> words = new List<string>(text.ToLower().Split(' '));
+            Dictionary<string, int> wordsNumber = new Dictionary<string, int>();
+            foreach (string word in words)
+            {
+                if (!wordsNumber.ContainsKey(word))
+                    wordsNumber.Add(word, 1);
+                else
+                    wordsNumber[word] = wordsNumber[word] + 1;
+            }
+            return wordsNumber;
         }
     }
 }
