7378acf24ede54e54c6c77e719f87d1e58e9fd0a
Ihor <igormykhno@gmail.com>
2021-10-21 12:40:56 +0300

    create project

---
+++ Word_frequency/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Word_frequency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
